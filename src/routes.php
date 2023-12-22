83b3e4fb9a0469446bd30ba2453fa6c7a6e9679e
Muchamad Ishaq Z <ishaq.muchamad@gmail.com>
2023-12-22 16:40:49 +0700

    update

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 
 use Slim\App;
 use Slim\Http\Request;
 use Slim\Http\Response;
-
 
 return function (App $app) {
 
@@ -12,96 +14,131 @@ return function (App $app) {
     $app->get('/[{name}]', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
-
         // Render index view
         return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
-    // POLI
-    $app->group('/poli', function () use ($app) {
-        $app->get("/", function (Request $request, Response $response) {
-            $sql = "SELECT * FROM poli";
-            $stmt = $this->db->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->fetchAll();
-            return $response->withJson(
-                ["status" => "success", "data" => $result],
-                200
-            );
-        });
 
-        $app->get("/{id}", function (Request $request, Response $response, $args) {
-            $id = $args["id"];
+    // POLI
+    $app->get("/poli/", function (Request $request, Response $response){
+        $sql = "SELECT * FROM poli";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $mainCount=$stmt->rowCount();
+        $result = $stmt->fetchAll();
+        if($mainCount==0) {
+            return $this->response->withJson([
+                'status' => 'error',
+                'message' => 'no result data.'],200);
+        }
+        return $response
+        ->withJson([
+            "status" => "success",
+            "data" => $result], 200);
+        });
+    $app->get("/poli/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        $sql = "SELECT * FROM poli WHERE id =:id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute([":id" => $id]);
+        $mainCount=$stmt->rowCount();
+        $result = $stmt->fetch();
+        if($mainCount==0) {
+            return $this->response->withJson([
+                'status' => 'error', 'message' =>
+                'no result data.'],200);
+        }
+        return $response->withJson([
+            "status" => "success",
+            "data" => $result], 200);
+    });
+
+    $app->post("/poli/", function (Request $request, Response $response){
+        $new_poli = $request->getParsedBody();
+        $sql = "INSERT INTO poli (nama_poli) VALUE (:nama_poli)";
+        $stmt = $this->db->prepare($sql);
+        $data = [
+            ":nama_poli" => $new_poli["nama_poli"]
+        ];
+
+        if($stmt->execute($data))
+          {
+             // get data last id
+            $id = $this->db->lastInsertId();
             $sql = "SELECT * FROM poli WHERE id =:id";
             $stmt = $this->db->prepare($sql);
             $stmt->execute([":id" => $id]);
             $result = $stmt->fetch();
-            return $response->withJson(
-                ["status" => "success", "data" => $result],
-                200
-            );
-        });
-
-        $app->get("/search/", function (Request $request, Response $response) {
-            $keyword = $request->getQueryParam("keyword");
-            $sql = "SELECT * FROM poli WHERE nama_poli LIKE '%$keyword%'";
-            $stmt = $this->db->prepare($sql);
-            $data = [":nama_poli" => $keyword];
-            $stmt->execute($data);
-            $result = $stmt->fetchAll();
-            return $response->withJson(
-                ["status" => "success", "data" => $result],
-                200
-            );
-        });
-
-        $app->post("/", function (Request $request, Response $response) {
-            $new_poli = $request->getParsedBody();
-            $sql = "INSERT INTO poli (nama_poli) VALUE (:nama_poli)";
-            $stmt = $this->db->prepare($sql);
-            $data = [
-                ":nama_poli" => $new_poli["nama_poli"],
-            ];
-            if ($stmt->execute($data)) {
-                return $response->withJson(
-                    ["status" => "success", "data" => "1"],
-                    200
-                );
-            }
-            return $response->withJson(["status" => "failed", "data" => "0"], 200);
-        });
-
-        $app->put("/{id}", function (Request $request, Response $response, $args) {
-            $id = $args["id"];
-            $new_poli = $request->getParsedBody();
-            $sql = "UPDATE poli SET nama_poli=:nama_poli WHERE id=:id";
-            $stmt = $this->db->prepare($sql);
-            $data = [
-                ":id" => $id,
-                ":nama_poli" => $new_poli["nama_poli"],
-            ];
-            if ($stmt->execute($data)) {
-                return $response->withJson(
-                    ["status" => "success", "data" => "1"],
-                    200
-                );
-            }
-            return $response->withJson(["status" => "failed", "data" => "0"], 200);
-        });
-
-        $app->delete("/{id}", function (Request $request, Response $response, $args) {
-            $id = $args["id"];
-            $sql = "DELETE FROM poli WHERE id=:id";
-            $stmt = $this->db->prepare($sql);
-            $data = [
-                ":id" => $id,
-            ];
-            if ($stmt->execute($data)) {
-                return $response->withJson(
-                    ["status" => "success", "data" => "1"],
-                    200
-                );
-            }
-            return $response->withJson(["status" => "failed", "data" => "0"], 200);
-        });
+           return $response->withJson(
+                ["status" => "success",
+                "data" => $result
+            ], 200);
+          }
+        return $response->withJson(
+            ["status" => "failed",
+            "data" => "error insert poli"], 200);
     });
+
+    $app->put("/poli/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        $new_poli = $request->getParsedBody();
+        $sql = "UPDATE poli SET nama_poli=:nama_poli WHERE id=:id";
+        $stmt = $this->db->prepare($sql);
+
+        $data = [
+            ":id" => $id,
+            ":nama_poli" => $new_poli["nama_poli"]
+        ];
+        if($stmt->execute($data)){
+            // get response
+             $sql = "SELECT * FROM poli WHERE id =:id";
+             $stmt_r = $this->db->prepare($sql);
+             $stmt_r->execute([":id" => $id]);
+             $result = $stmt_r->fetch();
+            return $response->withJson(
+                [
+                "status" => "success",
+                "data" => $result], 200);
+        }
+
+        return $response->withJson([
+            "status" => "failed",
+            "data" => "error Update poli"], 200);
+    });
+
+    $app->delete("/poli/{id}", function (Request $request, Response $response, $args){
+        $id = $args["id"];
+        // cari data
+        $sql = "SELECT * FROM poli WHERE id =:id";
+        $get_stmt = $this->db->prepare($sql);
+        $get_stmt->execute([":id" => $id]);
+        $result = $get_stmt->fetch();
+
+        //hapus data
+        $sql = "DELETE FROM poli WHERE id=:id";
+        $stmt = $this->db->prepare($sql);
+        $data = [":id" => $id];
+
+        if($stmt->execute($data)) {
+            return $response->withJson([
+                "status" => "success",
+                "data" => $result ], 200);
+
+        }
+
+        return $response->withJson([
+                "status" => "failed",
+                "data" => "error hapus poli"], 200);
+    });
+
+    $app->get("/poli/search/", function (Request $request, Response $response, $args){
+        $keyword = $request->getQueryParam("keyword");
+        $sql = "SELECT * FROM poli WHERE nama_poli LIKE '%$keyword%'";
+        $stmt = $this->db->prepare($sql);
+        $data = [":nama_poli" => $keyword];
+        $stmt->execute($data);
+        $result = $stmt->fetchAll();
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+
+
 };
